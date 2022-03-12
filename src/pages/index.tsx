ab5f3340cb791365e2dac46929c15ee07e219cdf
Reinis Mazeiks <reinis.mazeiks@gmail.com>
2022-03-12 16:31:53 +0200

    Configure Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home({ allPostsData }) {
-  return (
-    <p>Hello world!</p>
-  )
+  return <p>Hello world!</p>;
 }
