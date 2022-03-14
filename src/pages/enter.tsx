757655f8cf2e57824ebc446e0d893d8a5bf4470a
Reinis Mazeiks <reinis.mazeiks@gmail.com>
2022-03-14 22:55:08 +0200

    Direct user to homepage if logged in

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,8 +1,17 @@
+import { useAuth } from "api/auth";
 import { AuthWidget } from "components/auth";
 import { Card } from "../atoms/card";
 import { LargePadding, WidthLimit } from "../atoms/layout";
 
+import Router from "next/router";
+
 export default function Enter() {
+  const [auth] = useAuth();
+
+  if (auth) {
+    Router.replace("/");
+  }
+
   return (
     <main>
       <WidthLimit>
